88bced3821db2065b3d45574499264559d905bb6
Bevane <bevane@live.com>
2025-07-15 10:50:10 +0700

    feat: add fileserver and healthz endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import express from "express";
+import { handlerReadiness } from "./api/readiness.js";
 
 const app = express();
-app.listen(8080);
+const PORT = 8080;
+
+app.use("/app", express.static("./src/app"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http//localhost:${PORT}`);
+});
+
+app.get("/healthz", handlerReadiness);
