5430d76c942ad879bfd8df36c176402b5084c0d8
Bevane <bevane@live.com>
2025-09-21 13:07:29 +0700

    feat: add makejwt function

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,4 +1,5 @@
 import { hash, compare } from "bcrypt";
+import { JwtPayload, sign } from "jsonwebtoken";
 
 export async function hashPassword(password: string): Promise<string> {
   const saltRounds = 10;
@@ -10,4 +11,21 @@ export async function checkPasswordHash(
   hash: string,
 ): Promise<boolean> {
   return compare(password, hash);
+}
+
+export function makeJWT(
+  userID: string,
+  expiresIn: number,
+  secret: string,
+): string {
+  type Payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
+  const currentTime = Math.floor(Date.now() / 1000);
+  const payload: Payload = {
+    iss: "chirpy",
+    sub: userID,
+    iat: currentTime,
+    exp: currentTime + expiresIn,
+  };
+  const token = sign(payload, secret);
+  return token;
 }
