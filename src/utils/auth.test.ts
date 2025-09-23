d004c28fadd6ef7809d0a4d5ba2572536a76a8b2
Bevane <bevane@live.com>
2025-09-23 16:09:33 +0700

    test: add tests for hash function

--- src/utils/auth.test.ts
+++ src/utils/auth.test.ts
@@ -1,2 +1,29 @@
 import { describe, it, expect, beforeAll } from "vitest";
-import { makeJWT, validateJWT } from "./auth";
+import { checkPasswordHash, hashPassword, makeJWT, validateJWT } from "./auth";
+
+describe("Password Hashing", () => {
+  const password1 = "correctpass!";
+  const password2 = "anotherpass$";
+  let hash1: string;
+  let hash2: string;
+
+  beforeAll(async () => {
+    hash1 = await hashPassword(password1);
+    hash2 = await hashPassword(password2);
+  });
+
+  it("should return true for the correct password and hash", async () => {
+    const result = await checkPasswordHash(password1, hash1);
+    expect(result).toBe(true);
+  });
+
+  it("should return false for the correct password and wrong hash", async () => {
+    const result = await checkPasswordHash(password1, hash2);
+    expect(result).toBe(false);
+  });
+
+  it("should return false for the wrong password and correct hash", async () => {
+    const result = await checkPasswordHash(password2, hash1);
+    expect(result).toBe(false);
+  });
+});
